d37ad6e7c0e167ac6c97303160dd7e99b9826614
Quit108 <emontingelli@gmail.com>
2026-03-11 15:02:33 -0400

    added button functionality, html structure to contact and about us pages, event listeners and images for social div

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,3 +1,34 @@
 const searchBtn = document.getElementById('btnSearch');
 const resetBtn = document.getElementById('btnReset');
+const contactBtn = document.getElementById('contactBtn');
 
+function clearSearch() {
+    document.getElementById('searchInput').value = '';
+    document.getElementById('searchResults').innerHTML = '';
+}
+
+function search() {
+    const input = document.getElementById('searchInput').value;
+    const resultDiv = document.getElementById('searchResults');
+
+    fetch (travel_recommendation_api.json)
+        .then (response => response.json())
+        .then (data => {
+            const result = data.find(item => item.name.toLowerCase() === input OR input.slice(0, (input.length-1)));
+
+            if (result && result === "countries") {
+
+                resultDiv.innerHTML += `<h2>${countries[1].name}</h2>`;
+                resultDiv.innerHTML += `<img src="${countries[1].imageUrl}" alt="hjh">`;
+            } else {
+                resultDiv.innerHTML = 'Condition not Found.';
+            }
+        })
+        .catch(error => {
+            console.error('Error', error);
+            resultDiv.innerHTML = 'An Error Occured while fetching Data.';
+        });
+}
+
+resetBtn.addEventListener('click', clearSearch);
+searchBtn.addEventListener('click', search);
