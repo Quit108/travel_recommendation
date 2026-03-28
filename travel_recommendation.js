9b1c3541febce5d2fae4ca509d14bcdbe8ea2e3c
Quit108 <emontingelli@gmail.com>
2026-03-28 12:48:13 -0400

    added images and started working on js functionalities

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,1 +1,13 @@
-test
+const searchBtn = document.getElementById('searchBtn');
+
+function search() {
+    const input = document.getElementById('destination');
+    const resultDiv = document.getElementById('searchDiv');
+    resultDiv.innerHTML = '';
+
+    fetch ('/travel_recomendation_api.json')
+
+}
+
+
+btnSearch.addEventListener('click', searchCondition);
